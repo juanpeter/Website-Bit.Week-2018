0a96ef3c84e582afc476c6c4141d2eff8d862f0a
João Pedro <juanpeterisrael@gmail.com>
2018-10-03 22:34:31 -0300

    Scroll funciona

---
+++ README.md
@@ -0,0 +1,7 @@
+# Website-Bit.Week-2018
+
+Repositório para o site da Bit.Week com temática X-men!
+
+As bibliotecas usadas para este site são:
+
+[Bootstrap](http://getbootstrap.com/)
---
+++ controller.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('div.link1').click(function() {
+    $('html, body').animate({
+      scrollTop: $("div.area1").offset().top
+    }, 1000)
+  }), 
+    $('div.link2').click(function (){
+      $('html, body').animate({
+        scrollTop: $("div.area2").offset().top
+      }, 1000)
+    }),
+    $('div.link3').click(function (){
+      $('html, body').animate({
+        scrollTop: $("div.area3").offset().top
+      }, 1000)
+    })
+    $('div.link4').click(function (){
+        $('html, body').animate({
+          scrollTop: $("div.area4").offset().top
+        }, 1000)
+    })
+    $('i.seta').click(function (){
+    $('html, body').animate({
+        scrollTop: $("div.menu-top").offset().top
+    }, 1000)
+    })
+  });
