82c7b70363a87698ccb62df8d6255536cb8a2e5c
João Pedro <juanpeterisrael@gmail.com>
2018-10-08 13:19:49 -0300

    "Menu Fixo com bootstrap 3!"

--- controller.js
+++ controller.js
@@ -1,32 +1,33 @@
 $(document).ready(function () {
-    $('div.link1').click(function() {
+
+
+/*Scroll para as divs!*/
+    $('.link1').click(function() {
     $('html, body').animate({
-      scrollTop: $("div.area1").offset().top
+      scrollTop: $(".area1").offset().top-50
     }, 1000)
   }), 
-    $('div.link2').click(function (){
+    $('.link2').click(function (){
       $('html, body').animate({
-        scrollTop: $("div.area2").offset().top
+        scrollTop: $("div.area2").offset().top-50
       }, 1000)
     }),
-    $('div.link3').click(function (){
+    $('.link3').click(function (){
       $('html, body').animate({
-        scrollTop: $("div.area3").offset().top
+        scrollTop: $("div.area3").offset().top-50
       }, 1000)
     })
-    $('div.link4').click(function (){
+    $('.link4').click(function (){
         $('html, body').animate({
-          scrollTop: $("div.area4").offset().top
+          scrollTop: $("div.area4").offset().top-50
         }, 1000)
-    })
-    $('i.seta').click(function (){
-    $('html, body').animate({
-        scrollTop: $("div.menu-top").offset().top
-    }, 1000)
     })
     $('.home').click(function (){
       $('html, body').animate({
-          scrollTop: $("div.menu-top").offset().top
+          scrollTop: $("div.main").offset().top-50
       }, 1000)
       })
-  });
+
+/*Collapse para o menu hamburguer quando clicar fora*/
+
+});
